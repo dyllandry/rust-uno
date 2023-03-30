8583f47fd581d7101ea29ba0b7321ede90136ef9
dylan <dylanglandry@gmail.com>
2023-03-30 19:06:56 -0400

    use rand crate to shuffle deck

--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # todo
 
-- shuffle (can be a util that shuffles vecs of anything)
 - player
 - deck
 - discard
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod card;
 mod game;
 
 use crate::game::create_deck;
+use rand::{seq::SliceRandom, thread_rng};
 
 fn main() {
-    let deck = create_deck();
+    let mut deck = create_deck();
+    deck.shuffle(&mut thread_rng());
     for card in &deck {
         println!("{}", card);
     }
